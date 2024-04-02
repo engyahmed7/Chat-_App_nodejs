5119a11092042f0144531652aa1a77ba7c87b291
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-04-02 15:01:24 +0200

    feat: implement real time chat app

---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(7000, () => {
+  console.log('http://localhost:7000');
+});
